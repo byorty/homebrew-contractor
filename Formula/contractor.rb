3e4c04e9ed948791c7ab2cdb14e34fcf55fe25b3
Alexey Solomonov <byorty@mail.ru>
2022-01-18 23:07:44 +0300

    added description

--- Formula/contractor.rb
+++ Formula/contractor.rb
@@ -1,12 +1,12 @@
 class Contractor < Formula
   homepage "https://github.com/byorty/contractor"
-  desc ""
+  desc "Reads the description document and generates test cases that will ensure that your application is compliant with its description"
   url "https://github.com/byorty/contractor/releases/download/0.0.17/contractor-0.0.17-darwin-arm64.tar.gz"
   sha256 "d2a76450ea35149d8fc12a087a278b2a07e9307af8ac0edba07056cd45e415e6"
   license "MIT"
 
   def install
-    system "./configure", *std_configure_args, "--disable-silent-rules"
+    bin.install "contractor"
   end
 
   test do
