928fb3020cc73b0dc154e1f27295cd7d5eeb83f4
Alexey Solomonov <byorty@mail.ru>
2022-01-19 01:19:52 +0300

    updated formula

--- contractor.rb
+++ contractor.rb
@@ -1,8 +1,8 @@
 class Contractor < Formula
   homepage "https://github.com/byorty/contractor"
   desc "Reads the description document and generates test cases that will ensure that your application is compliant with its description"
-  url "https://github.com/byorty/contractor/releases/download/0.0.17/contractor-0.0.17-darwin-arm64.tar.gz"
-  sha256 "d2a76450ea35149d8fc12a087a278b2a07e9307af8ac0edba07056cd45e415e6"
+  url "https://github.com/byorty/contractor/releases/download/0.0.22/contractor_0.0.22_Darwin_arm64.tar.gz"
+  sha256 "1d4f287fe3808a9fe27abfe0b7d028f4a58c19dd3903f10a9a89e167f7f70407"
   license "MIT"
 
   def install
