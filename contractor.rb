dc21c3108dfcab14ce09f06a7e08d43249106019
Alexey Solomonov <byorty@mail.ru>
2022-04-26 09:37:59 +0300

    contractor refs/tags/0.0.54

--- contractor.rb
+++ contractor.rb
@@ -1,8 +1,8 @@
 class Contractor < Formula
   homepage "https://github.com/byorty/contractor"
   desc "Reads the description document and generates test cases that will ensure that your application is compliant with its description"
-  url "https://github.com/byorty/contractor/releases/download/0.0.50/contractor_0.0.50_darwin_amd64.tar.gz"
-  sha256 "b0bce414d0219d7ad1376d49e4bd8fa2b149cb62aee89222b9dcb5ac11a0806b"
+  url "https://github.com/byorty/contractor/releases/download/0.0.54/contractor_0.0.54_darwin_amd64.tar.gz"
+  sha256 "465d2cae16e04570e333d1c5946becba586ee82332bb1160508870401f54719c"
   license "MIT"
 
   def install
